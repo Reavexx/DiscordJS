fd6cd9f19d3e7cb1545cc79e1d5500e282161ed7
Dines Nimalthas <56248619+Reavexx@users.noreply.github.com>
2022-12-19 13:13:33 +0100

    Add files via upload

---
+++ DiscordJS/index.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const client = new Discord.Client();
+
+const prefix = "?";
+
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Pong! Diese Nachricht hat eine Latenz von ${timeTaken}ms.`);
+  }
+
+  else if (command === "summary") {
+    const numArgs = args.map(x => parseFloat(x));
+    const sum = numArgs.reduce((counter, x) => counter += x);
+    message.reply(`Die Zusammenfassung des Servers wird hier aufgelistet.`);
+  }
+
+  else if (command === "help") {
+    const numArgs = args.map(y => parseFloat(y));
+    message.reply('In diesem Log finden Sie die Hilfestellung zu dem Reavex Server');
+  }
+});
+
+client.login(config.MTA1NDM2NDkyMjQxMjI4NTk5Mg.GhyLD-.JkfudHCoKmy9uIa2tMlnfDYlOaq-Yib0HamIFA);
